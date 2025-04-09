f7b7e043bfad860c1bc48f42948de3b2b03f4cb8
Akash Jadhav <102428182+jadhavakashofficial@users.noreply.github.com>
2025-04-10 04:53:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+function daysOfAYear(year) {
+  if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+    return 366;
+  } else {
+    return 365;
+  }
+}
+
+console.log(daysOfAYear(2022)); // 365
+console.log(daysOfAYear(2024)); // 366
+console.log(daysOfAYear(1900)); // 365
+console.log(daysOfAYear(2000)); // 366
